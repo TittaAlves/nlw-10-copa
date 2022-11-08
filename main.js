102a4437ec5204a9fa47b0e8d3fa9589d358f3ee
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-11-07 22:48:37 -0300

    Alteração título README e retirado animação do card

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function createGame(player1, hour, player2, group = "x") {
   `
 }
 
-let delay = -0.3
+let delay = -0.0
 function createCard(date, day, games) {
-  delay = delay + 0.3
+  delay = delay + 0.1
   return `
   <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
